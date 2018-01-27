6b53efd30ea935abc3dfdf2c0bf448418eeabe5e
Joshua Loschen <josh.loschen@gmail.com>
2018-01-27 16:42:06 -0600

    Figured out getting data, now adding locations.

--- js/app.js
+++ js/app.js
@@ -4,14 +4,23 @@
 
     var EauClaireLocations = [
         {title: 'Highland Fitness', fourSquareId: '4c88f0c9da5da1cdb60838e9'},
-        {title: 'Golden Dragon Chinese Restaurant', fourSquareId: '4e4e5a14bd4101d0d7a85286'},
-        {title: 'UW- Eau Claire', fourSquareId: '4da600c40c53054ed7b4df8e'},
+        //{title: 'Golden Dragon Chinese Restaurant', fourSquareId: '4e4e5a14bd4101d0d7a85286'},
+        {title: 'UW-Eau Claire', fourSquareId: '4da600c40c53054ed7b4df8e'},
+        //{title: 'Wagner\'s Lanes', fourSquareId: '4b83363bf964a5209afd30e3'},
+        {title: 'The Informalist', fourSquareId: '57229631cd1010835c5139a0', defaultUrl: 'http://theinformalist.com'},
+        {title: 'Lazy Monk Brewing', fourSquareId: '4eb582a4e5e8743705159e38'},
+        {title: 'Carson Park', fourSquareId: '4bd62e257b1876b0e42f8c86', defaultUrl:'https://northwoodsleague.com/eau-claire-express/team/ballpark/'}, // no good url's
+        //{title: 'The District Company', fourSquareId: '51bb8cd0498e9ac0b653d06f'},
+        {title: 'Eau Claire YMCA', fourSquareId: '4c4b6fa25609c9b6ae5b1d91'},
+        {title: 'Eau Claire Children\'s Museum', fourSquareId: '4c5adf90d3aee21e65b76b55', defaultUrl:'http://www.childrensmuseumec.com'},
+        {title: 'Acoustic Cafe', fourSquareId: '4b7c4b8ff964a520668a2fe3'}
+        //{title: 'Dean & Sue\'s Bar & Grill', fourSquareId: '4e18dac5d1648b8348372829'}
     ];
     var largeInfowindow; 
 
     function initMap() {
         map = new google.maps.Map(document.getElementById('map'), {
-            center: {lat:44.817178, lng: -91.511481},//Garner Iowa
+            center: {lat:44.817178, lng: -91.511481},//Eau Claire WI
             zoom: 14,
             mapTypeControl: false
         });
@@ -122,24 +131,33 @@ class EauClairePin{
         this.getFourSquareData(this.fourSquareId, this.title, this.index);
     }
 
-    openInfoWindow(){
-        
-        if (largeInfowindow.marker != this.marker) {
+    // openInfoWindow(param1){
+    //     //JSON.stringify(param1)
+    //     console.log("1"+ param1);
+    //     console.log("2"+ param2.fourSquareId);
+    //     if(this === param2){
+    //         console.log("yahtzee");
+    //     }
+    // }
+     openInfoWindow(marker){
+        if (largeInfowindow.marker != marker) {
             
-            largeInfowindow.marker = this.marker;
+            largeInfowindow.marker = marker;
             var content = `<div>
-                                <div>${this.marker.title}</div>
-                                <div>${this.url}</div>
+                                <div>${marker.title}</div>
+                                <div>
+                                <a href="${this.url}">${this.url}</a></div>
+                                <div><img src="${this.bestPhotoURL}">
                             </div>`;
 
             largeInfowindow.setContent(content);
-            largeInfowindow.open(map, this.marker);
+            largeInfowindow.open(map, marker);
 
             largeInfowindow.addListener('closeclick', function() {
                 largeInfowindow.marker = null;
             });
         }else{
-            console.log("they are equal" + largeInfowindow.marker + " marker" + this.marker);
+            console.log("they are equal" + largeInfowindow.marker + " marker" + marker);
         }
     }
 
@@ -153,16 +171,30 @@ class EauClairePin{
             
             this.url = res.response.venue.url;
 
+            if(res.response.venue.bestPhoto){
+                this.bestPhotoURL = res.response.venue.bestPhoto.prefix + "100x100" + res.response.venue.bestPhoto.suffix;
+            }
+            
+
+            
             this.marker = new google.maps.Marker({
                 position: res.response.venue.location,
                 title: this.title,
                 animation: google.maps.Animation.DROP,
                 id: this.index
             });
-            console.log("this inside promise" + this);
+            
             markers.push(this.marker);
         
-            this.marker.addListener('click', this.openInfoWindow);/*function() {
+            // this.marker.addListener('click', function() {
+            //     populateInfoWindow(this, largeInfowindow);
+            // });
+            var pin = this;
+            this.marker.addListener('click', function(){
+                pin.openInfoWindow(this);
+            });
+
+            /*function() {
                 if (largeInfowindow.marker != marker) {
                     
                     largeInfowindow.marker = marker;
@@ -186,6 +218,10 @@ class EauClairePin{
             alert(err);
         });
     }
+}
+
+function testThing(param1, param2){
+
 }
 
 function addMarkerToMap(position, title, i){
