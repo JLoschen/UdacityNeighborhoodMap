6dba61ff3472be9655b3da1fa5487ee190474d68
Joshua Loschen <josh.loschen@gmail.com>
2018-01-30 20:15:09 -0600

    Code cleanup and added comments.

--- js/app.js
+++ js/app.js
@@ -1,20 +1,21 @@
-    var map;
-    // Create a new blank array for all the listing markers.
-    var markers = [];
+  
 
     var EauClaireLocations = [
-        { title: 'UW-Eau Claire', fourSquareId: '4d7e7bb795c1a143f65dd2f2'},
-        { title: 'The Informalist', fourSquareId: '57229631cd1010835c5139a0', defaultUrl: 'http://theinformalist.com'},
-        { title: 'Lazy Monk Brewing', fourSquareId: '4eb582a4e5e8743705159e38'},
-        { title: 'Carson Park', fourSquareId: '4bd62e257b1876b0e42f8c86', defaultUrl:'https://www.visiteauclaire.com/things-to-do/outdoors/parks/carson/'}, 
-        { title: 'Eau Claire Children\'s Museum', fourSquareId: '4c5adf90d3aee21e65b76b55', defaultUrl:'http://www.childrensmuseumec.com'},
-        { title: 'Acoustic Cafe', fourSquareId: '4b7c4b8ff964a520668a2fe3'},
-        { title: 'Phoenix Park', fourSquareId: '4b9d6507f964a52082a936e3' , defaultUrl:'https://www.visiteauclaire.com/listings/phoenix-park/1898/'},
-        { title: 'Banbury Place', fourSquareId: '4c979e15f7cfa1cd9202d015', defaultUrl:'http://www.banbury.com/'},
+        // { title: 'UW-Eau Claire', fourSquareId: '4d7e7bb795c1a143f65dd2f2'},
+        // { title: 'The Informalist', fourSquareId: '57229631cd1010835c5139a0', defaultUrl: 'http://theinformalist.com'},
+        // { title: 'Lazy Monk Brewing', fourSquareId: '4eb582a4e5e8743705159e38'},
+        // { title: 'Carson Park', fourSquareId: '4bd62e257b1876b0e42f8c86', defaultUrl:'https://www.visiteauclaire.com/things-to-do/outdoors/parks/carson/'}, 
+        // { title: 'Eau Claire Children\'s Museum', fourSquareId: '4c5adf90d3aee21e65b76b55', defaultUrl:'http://www.childrensmuseumec.com'},
+        // { title: 'Acoustic Cafe', fourSquareId: '4b7c4b8ff964a520668a2fe3'},
+         { title: 'Phoenix Park', fourSquareId: '4b9d6507f964a52082a936e3' , defaultUrl:'https://www.visiteauclaire.com/listings/phoenix-park/1898/'},
+         { title: 'Banbury Place', fourSquareId: '4c979e15f7cfa1cd9202d015', defaultUrl:'http://www.banbury.com/'},
         { title: 'The Nucleus', fourSquareId: '4720b2dcf964a520c84b1fe3'}
     ];
     var largeInfowindow; 
     var mainViewModel;
+    var map;
+    // Create a new blank array for all the listing markers.
+    var markers = [];
 
     function initMap() {
         map = new google.maps.Map(document.getElementById('map'), {
@@ -30,15 +31,16 @@
     }
 
 class MapViewModel{
-    constructor(EauClaireLocations){
-        this.allLocations = EauClaireLocations;
-        this.searchString = ko.observable('');
-    
-        this.locations = [];
-        for(let i = 0; i < EauClaireLocations.length; i++){
-            this.locations.push(new EauClairePin(EauClaireLocations[i], i));
-        }
 
+    constructor(EauClaireLocations){
+        this.locations = [];
+        EauClaireLocations.forEach(function(loc){
+            this.locations.push(new EauClairePin(loc));
+        },this);
+
+        //compute filtered list on left side each time the search string changes
+        //https://stackoverflow.com/questions/29551997/knockout-search-filter
+        this.searchString = ko.observable('');
         this.filterPins = ko.computed(function () {
             var search = this.searchString().toLowerCase();
             return ko.utils.arrayFilter(this.locations, function (pin) {
@@ -47,11 +49,9 @@ class MapViewModel{
         }, this);
 
         this.selectedModel = this.locations[0];
-        
-        this.selectedModel.isSelected(false);
-
         this.selectedModelId = ko.observable(this.selectedModel.fourSquareId);
 
+        this.selectedModelId('');
         //when selected pin changes then change the images showing on side.
         this.selectedImages = ko.computed(function(){
             var id = this.selectedModelId();
@@ -63,26 +63,30 @@ class MapViewModel{
                 return result.photos;
             }
         },this);
-
-        this.selectedModelId('');
-        showListings();
     }
 
     openInfoWindow(marker, clickedPin){
-        if (largeInfowindow.marker != marker) {
+         //if not already showing this marker
+         if (largeInfowindow.marker != marker) {
             
+            //unselect previous marker
             this.selectedModel.marker.setIcon('img/red pin.png');
             this.selectedModel.isSelected(false);
 
+            //this triggers knockout computed images property to update
             this.selectedModelId(clickedPin.fourSquareId);
+
             largeInfowindow.marker = marker;
-            
             largeInfowindow.setContent(clickedPin.infoWindowContent);
             largeInfowindow.open(map, marker);
 
             largeInfowindow.addListener('closeclick', function() {
                 largeInfowindow.marker = null;
             });
+            
+            marker.setAnimation(google.maps.Animation.BOUNCE);
+            marker.setIcon('img/green pin.png');
+            clickedPin.isSelected(true);
 
             //stop bouncing after 1.4 seconds (about 2 bounces)
             setTimeout((function() {
@@ -93,21 +97,17 @@ class MapViewModel{
 }
 
 class EauClairePin{
-    constructor(loc, index){
-        this.self = this;
+    constructor(loc){
         this.title = loc.title;
         this.defaultUrl = loc.defaultUrl;
         this.fourSquareId = loc.fourSquareId;
-        this.index = index;
-        this.getFourSquareData(this.fourSquareId, this.title, this.index);
+        this.getFourSquareData(this.fourSquareId, this.title);
         this.isSelected = ko.observable(false);
     }
 
+    //the next 3 functions are list item events
     onClick(){
         mainViewModel.openInfoWindow(this.marker, this);
-        this.marker.setAnimation(google.maps.Animation.BOUNCE);
-        this.marker.setIcon('img/green pin.png');
-        this.isSelected(true);
     }
 
     onHoverOver(){
@@ -119,14 +119,14 @@ class EauClairePin{
     }
 
     setIconIfValid(icon){
+        //only set icon if is loaded already and don't change the selected icon from green
         if(this.marker && this.fourSquareId !== mainViewModel.selectedModelId()){
             this.marker.setIcon(icon);
         }
     }
 
-    getFourSquareData(fourSquareId, title ,index){
-        //var url = 'https://api.foursquare.com/v2/venues/' + fourSquareId + '?client_id=H3U143RXVFSPJE0XYGV2C2LUTZLPJVX4O3D4XKEKINMVMORJ&client_secret=GD2SHFG2LB1KWLZ45PUWKHGQ3MAW102ML3CVLOJDGKJV2UHB&v=20170413';
-        var url = 'https://api.foursquare.com/v2/venues/' + fourSquareId + '?client_id=2G4BOAVMDDTBVKZOU0WI0IBXSQOCMDTIOWZCKXS4XO1RAC0R&client_secret=3UZMRJ1XEB1WDHZROFUCCIGDJCFMWPVRG5J4FFDWVDNHEV4K&v=20170413';
+    getFourSquareData(fourSquareId, title ){
+        var url = 'https://api.foursquare.com/v2/venues/' + fourSquareId + '?client_id=H3U143RXVFSPJE0XYGV2C2LUTZLPJVX4O3D4XKEKINMVMORJ&client_secret=GD2SHFG2LB1KWLZ45PUWKHGQ3MAW102ML3CVLOJDGKJV2UHB&v=20170413';
         
         fetch(url)
         .then((data) => data.json())
@@ -146,11 +146,10 @@ class EauClairePin{
                 this.bestPhotoURL = venue.bestPhoto.prefix + "100x100" + venue.bestPhoto.suffix;
             }
 
-            var url = this.defaultUrl;
-            if(venue.url){
-                url = venue.url;
-            }
+            //some places like parks didn't have websites registered with foursquare so fall back to defaultUrl
+            var url = venue.url ? venue.url : this.defaultUrl;
 
+            //no good way to bind the google maps infowindow so cheating on mvvm by hardcoding view.
             this.infoWindowContent 
                     =  `<div style="max-width:360px;">
                             <h3 class="venue-name">${venue.name}</h3>
@@ -171,32 +170,22 @@ class EauClairePin{
                 position: venue.location,
                 title: this.title,
                 icon: 'img/red pin.png',
-                //title:venue.name,
-                animation: google.maps.Animation.DROP,
-                id: this.index
+                animation: google.maps.Animation.DROP,//drop in initially, animation is switched to bounce later
             });
             
             markers.push(this.marker);
 
-            var pin = this;
+            var pin = this;//capturing the EauClairePin Model so can pass to openInfoWIndow function
+
             this.marker.addListener('click', function(){
-                mainViewModel.openInfoWindow(this,pin);
-                this.setAnimation(google.maps.Animation.BOUNCE);
-                this.setIcon('img/green pin.png');
+                mainViewModel.openInfoWindow(this, pin);
             });
-        
+
+            //setMap makes the marker show up on the map
             this.marker.setMap(map);
         })
         .catch((err) => {
             alert(err);
         });
-    }
-}
-
-function showListings() {
-    var bounds = new google.maps.LatLngBounds();
-    for (var i = 0; i < markers.length; i++) {
-        markers[i].setMap(map);
-        bounds.extend(markers[i].position);
     }
 }
